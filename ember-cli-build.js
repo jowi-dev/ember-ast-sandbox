4b51e2a03ab7f719260206ed6bbf65f1614de6c8
joey8williams <joey8williams@gmail.com>
2020-10-11 12:29:33 -0400

    setup process. Mirage, svg-jar, bundle-size, fastboot, css mostly complete

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,15 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
+    'ember-ast-hot-load':{
+      enabled: true
+    },
+
+    stylelint: {
+      linterConfig: {
+        syntax: 'css'
+      }
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
